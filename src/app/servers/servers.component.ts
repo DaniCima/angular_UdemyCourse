25d11bf1b34435ebf09c7fcda893497946dcbb79
DESKTOP-SBQEKMV\Daniela <daniela.cima@es.gestamp.com>
2023-01-30 17:42:52 +0100

    36

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `<app-server></app-server>
-    <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  // template: `<app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created yet';
+  serverName = 'TestServer';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
 }
